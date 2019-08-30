404e2e7b6c1430ab5a685b1188c0e222fa9278ff
m-eydimorad <m.eidimorad@gmail.com>
2019-08-31 02:11:31 +0430

    init specflow plugin

---
+++ Code/src/Plugins/Suzianna.SpecFlowPlugin/SuziannaTestTracerWrapper.cs
@@ -0,0 +1,50 @@
+﻿using Suzianna.Reporting;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Bindings;
+using TechTalk.SpecFlow.Bindings.Reflection;
+using TechTalk.SpecFlow.BindingSkeletons;
+using TechTalk.SpecFlow.Configuration;
+using TechTalk.SpecFlow.Tracing;
+
+namespace Suzianna.SpecFlowPlugin
+{
+    internal class SuziannaTestTracerWrapper : TestTracer,ITestTracer
+    {
+        protected readonly IReporter Reporter;
+        public SuziannaTestTracerWrapper(IReporter reporter,ITraceListener traceListener, IStepFormatter stepFormatter,
+          IStepDefinitionSkeletonProvider stepDefinitionSkeletonProvider, SpecFlowConfiguration specFlowConfiguration)
+          : base(traceListener, stepFormatter, stepDefinitionSkeletonProvider, specFlowConfiguration)
+        {
+            Reporter = reporter;
+        }
+        void ITestTracer.TraceStep(StepInstance stepInstance, bool showAdditionalArguments)
+        {
+            TraceStep(stepInstance, showAdditionalArguments);
+            //StartStep(stepInstance);
+        }
+
+        private void StartStep(StepInstance stepInstance)
+        {
+            Reporter.StepStarted(stepInstance.StepContext.FeatureTitle, stepInstance.StepContext.ScenarioTitle, stepInstance.Text);
+        }
+
+        void ITestTracer.TraceStepDone(BindingMatch match, object[] arguments, TimeSpan duration)
+        {
+            TraceStepDone(match, arguments, duration);
+            //Reporter.MarkScenarioAsFailed
+        }
+        //public SuziannaTestTracerWrapper(IReporter Reporter)
+        //{
+        //    this.Reporter = Reporter;
+        //}
+
+        //public override void TraceStep(StepInstance stepInstance, bool showAdditionalArguments)
+        //{
+        //    //Reporter.StepStarted(stepInstance.StepContext.FeatureTitle, stepInstance.StepContext.ScenarioTitle, stepInstance.Text);
+        //}
+
+    }
+}
